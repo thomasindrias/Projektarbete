b608697c681a0fc70f41fffe11c1b66a13005584
Thomas Indrias Biniam <thoin216@student.liu.se>
2016-11-29 21:15:56 +0100

    src

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <title>main page</title>
+</head>
+
+<body>
+   <div id="searchbar">
+      <form method="post" action="search.php" id="search">
+          <input type="text" name="searchpost" placeholder="Search here">
+          <input type="submit" name="submit" value="Search">
+      </form>
+    </div>
+
+     <div id="toolbar"></div>
+     
+     <div id="homepageInfo">
+         <p>Under construction</p>
+     </div>
+    
+    <footer></footer>
+</body>
+</html>
